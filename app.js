3c4bccb259b8a00e92d917e97ae537ae3746fe5e
blakeparkinson <wizardplow@gmail.com>
2017-05-29 21:44:43 -0600

    Pushing up more changes

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-require('newrelic');
+//require('newrelic');
 
 var routes = require('./routes/index');
-var main = require('./routes/main');
 
 
 var app = express();
@@ -26,8 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/main', main);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/androidlink', cors(), (req, res) => {
+    var androidLinks = ['asbury.dpsk12.org'];
+    res.json({result: androidLinks});
+})
+
+router.get('/appversion', cors(), (req, res) => {
+    var version = {
+        ios: '1.1.1',
+        androind: '1.2.4'
+    }
+    res.json({result: version});
+})
 
 module.exports = router;
