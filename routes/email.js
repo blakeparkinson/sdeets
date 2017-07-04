213df6c803376f9113599d5e3541c3292f3b825b
blakeparkinson <wizardplow@gmail.com>
2017-07-04 00:40:37 -0600

    setting up the email api oauth

--- routes/email.js
+++ routes/email.js
@@ -1,11 +1,48 @@
 var nodemailer = require("nodemailer");
+var xoauth2 = require('xoauth2');
 
+
+var transporter = nodemailer.createTransport("SMTP",{
+    service: 'gmail',
+    auth: {
+    XOAuth2: {
+      user: process.env.GMAIL_UN, // Your gmail address.
+      clientId: process.env.GMAIL_CLIENT_ID,
+      clientSecret: process.env.GMAIL_SECRET,
+      refreshToken: process.env.GMAIL_REFRESH_TOKEN
+    }
+  }
+});
 
 module.exports = {
 
+    
     send:function( cb ){
 
-        cb('hi');
+            console.log(process.env);
+          var mailOptions = {
+
+            from: 'test', // sender address
+            to: 'wizardplow@gmail.com', // list of receivers
+            subject: 'testing', // Subject line
+            html: '<div>Hello World</div>'
+        };
+
+        // send mail with defined transport object
+    transporter.sendMail(mailOptions, function(error, info) {
+      console.log('info is: ' + info);
+        if (error) {
+            console.log('we got an error' + error);
+            cb(error);
+
+        } else {
+            console.log('email sent');
+            cb('hi');
+
+        }
+        transporter.close();
+    });
+
 
     }
 }
